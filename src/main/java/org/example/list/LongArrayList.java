e37ead8eb6542662be6e9a6345dffc512f1aad3b
apavlov <albert.pavlov@domrf.ru>
2024-08-20 16:12:45 +0300

    Методы + тесты

--- src/main/java/org/example/list/LongArrayList.java
+++ src/main/java/org/example/list/LongArrayList.java
@@ -2,7 +2,7 @@ package org.example.list;
 
 import java.util.Iterator;
 
-class LongArrayList implements LongList {
+public class LongArrayList implements LongList {
     private long[] values;
     private int capacity;
     private int size;
@@ -14,23 +14,29 @@ class LongArrayList implements LongList {
     }
 
     @Override
-    public void get(int index) {
-        throw new UnsupportedOperationException();
+    public long get(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return values[index];
     }
 
     @Override
     public void set(int index, long element) {
-        throw new UnsupportedOperationException();
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        values[index] = element;
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException();
+        return size == 0;
     }
 
     @Override
@@ -38,7 +44,6 @@ class LongArrayList implements LongList {
         if (size + 1 > capacity) {
             updateCapacity(capacity * 2);
         }
-        throw new UnsupportedOperationException();
     }
 
     @Override
@@ -46,18 +51,34 @@ class LongArrayList implements LongList {
         if (size + 1 > capacity) {
             updateCapacity(capacity * 2);
         }
-        throw new UnsupportedOperationException();
     }
 
     @Override
-    public void remove(int index) {
-        throw new UnsupportedOperationException();
+    public long remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        long removedValue = values[index];
+
+        for (int i = index; i < size - 1; i++) {
+            values[i] = values[i + 1];
+        }
+        size--;
+
+        return removedValue;
     }
 
+
     @Override
-    public void contains(long element) {
-        throw new UnsupportedOperationException();
+    public boolean contains(long element) {
+        for (int i = 0; i < size; i++) {
+            if (values[i] == element) {
+                return true;
+            }
+        }
+        return false;
     }
+
 
     @Override
     public Iterator<Long> iterator() {
@@ -71,5 +92,49 @@ class LongArrayList implements LongList {
         }
         values = valuesArray;
         capacity = newCapacity;
+    }
+
+    public static class ArrayRealization {
+        LongArrayList longList = new LongArrayList();
+        public void arrayAddElements() {
+
+            longList.add(0, 10);
+            longList.add(1, 20);
+            longList.add(2, 30);
+            longList.add(3, 40);
+            longList.add(4, 50);
+            longList.add(60);
+        }
+
+        public void arrayGetElement() {
+
+            longList.get(2);
+        }
+
+        public void arraySetElement() {
+
+            longList.set(2, 31);
+        }
+
+        public void arrayGetSize() {
+
+            longList.size();
+        }
+
+        public void arrayIsEmpty() {
+
+            longList.isEmpty();
+        }
+
+        public void arrayRemove() {
+
+            longList.remove(3);
+        }
+
+        public void arrayContains() {
+
+            longList.contains(60);
+        }
+
     }
 }
--- src/main/java/org/example/list/LongList.java
+++ src/main/java/org/example/list/LongList.java
@@ -3,7 +3,7 @@ package org.example.list;
 import java.util.Iterator;
 
 public interface LongList {
-    void get(int index);
+    long get(int index);
     void set(int index, long element);
 
     int size();
@@ -11,9 +11,9 @@ public interface LongList {
 
     void add(long element);
     void add(int index, long element);
-    void remove(int index);
+    long remove(int index);
 
-    void contains(long element);
+    boolean contains(long element);
 
     Iterator<Long> iterator();
 }
